5a72ad86918a972f05f369835021a6214ffdba51
aniket3323 <aniketghodke3323@gmail.com>
2024-07-31 09:33:32 +0530

    Added average calculation to LargestNumber program

--- LargestNumber.java
+++ LargestNumber.java
@@ -14,7 +14,9 @@ public class LargestNumber {
         int num3 = scanner.nextInt();
         
         int largest = Math.max(num1, Math.max(num2, num3));
+        double average = (num1 + num2 + num3) / 3.0;
         
         System.out.println("The largest number is: " + largest);
+        System.out.println("The average of the numbers is: " + average);
     }
 }
